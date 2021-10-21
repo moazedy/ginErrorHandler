3e7c47362c95bb246711ba9f4cf08ebe6f71530f
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-10-21 11:58:17 +0330

    adding needed comments

--- errorHandler.go
+++ errorHandler.go
@@ -2,11 +2,13 @@ package ginErrorHandler
 
 import "github.com/gin-gonic/gin"
 
+// GinError is a custom type for error to be handled in HTTP
 type GinError struct {
 	Message string `json:"error_message"`
 	Code    int    `json:"error_code"`
 }
 
+// implementing error type interface for GinError
 func (g *GinError) Error() string {
 	return g.Message
 }
@@ -19,6 +21,9 @@ func New(message string, code int) error {
 	}
 }
 
+// HandleTheError gets gin context refferenc and also an error as input arguments
+// and responses to HTTP client using the error parameters
+// TODO: allways call return, after calling this function in your scope.
 func HandleTheError(ctx *gin.Context, err GinError) {
 	ctx.JSON(err.Code, gin.H{
 		"error_message": err.Message,

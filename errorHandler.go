b0af7c4851a8322589c2c7e33654fcef33363e9c
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-10-21 11:09:09 +0330

    adding constructor fucntion for GinError

--- errorHandler.go
+++ errorHandler.go
@@ -11,6 +11,14 @@ func (g *GinError) Error() string {
 	return g.Message
 }
 
+// construction func to declare GinError as an error
+func New(message string, code int) error {
+	return &GinError{
+		Message: message,
+		Code:    code,
+	}
+}
+
 func HandleTheError(ctx *gin.Context, err GinError) {
 	ctx.JSON(err.Code, gin.H{
 		"error_message": err.Message,
